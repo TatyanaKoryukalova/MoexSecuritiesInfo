efff023936610fecd533a58189500c378a099725
TatyanaKoryukalova <tata.koryukalova@yandex.ru>
2022-12-10 00:48:24 +0500

    Initial commit

---
+++ src/main/java/com/moex/securitiesinfo/MoexSecuritiesInfoApplication.java
@@ -0,0 +1,13 @@
+package com.moex.securitiesinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoexSecuritiesInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoexSecuritiesInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moex/securitiesinfo/MoexSecuritiesInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moex.securitiesinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoexSecuritiesInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
